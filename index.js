8906ac9dbc166cc5e9db730bbae397e862b3393e
justakashk <akashkumar60907@gmail.com>
2026-01-02 23:00:56 +0530

    implement the feature, show main content of notes on a page named show

--- index.js
+++ index.js
@@ -15,10 +15,22 @@ app.get('/',function(req,res){
     });
 })
 
+// creating routes for readmore...
 
+app.get('/file/:filename', function(req,res){
+ 
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata});
+    })
+
+})
+
+
+
+// Creating Routes For showing files 
 
 app.post('/create', function(req,res){
-   fs.writeFile(`./files/${req.body.title.split(" ").join(" ")}.txt`, req.body.details , function(err) {})
+   fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`, req.body.details , function(err) {})
    res.redirect("/");
 })
 
